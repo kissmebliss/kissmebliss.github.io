f7565c45f7af1bb86bf16498f9ac835cdec6d5ac
kissmebliss <daily1987@naver.com>
2016-08-04 14:04:16 +0900

    sub_01.html

---
+++ js/js_01.js
@@ -0,0 +1,33 @@
+/**
+* javascript: 3 - 5 - 6(ecma 2015, ecma6) -7
+*/
+
+// 변수 : 변하는 값. 데이터를 담는 그릇. 
+
+
+var box = 'test';
+var box1 = 9;
+var text = '<p>Lorem ipsum dolor sit ament</p>';
+var tt = null;
+var tat;
+
+document.write('box'); //문자로 box
+document.write(box1); //숫자 9.
+document.write(text);
+
+/*
+number = 숫자;
+string = '문자';
+boolean = true / false ;
+null = 없다.
+undefined = 정의되지 않은;
+array =
+object =
+function =
+*/
+
+/*
+= 대입하겠다.
+== 같다(형분석 X)
+=== 같다.(형분석 O, 엄격하게 구분. 현시점에서 사용하는 비교.)
+*/
